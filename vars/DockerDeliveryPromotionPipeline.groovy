ebcfd75fcc8ae0d80b2c4947634b377864e42fe3
CloudEthix987 <payaldkharat@coep.sveri.ac.in>
2026-09-17 14:34:51 +0530

    Create DockerDeliveryPromotionPipeline.groovy

---
+++ vars/DockerDeliveryPromotionPipeline.groovy
@@ -0,0 +1,297 @@
+def call(body) {
+
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    pipeline {
+
+        agent any
+
+        environment {
+
+            registryURL = 'https://registry.hub.docker.com/'
+
+            dev_registry   = 'payalkharat/cloudethix-sample-nginx-dev'
+            qa_registry    = 'payalkharat/cloudethix-sample-nginx-qa'
+            stage_registry = 'payalkharat/cloudethix-sample-nginx-stage'
+            prod_registry  = 'payalkharat/cloudethix-sample-nginx-prod'
+
+            dev_dh_creds   = 'dh_cred_dev'
+            qa_dh_creds    = 'dh_cred_qa'
+            stage_dh_creds = 'dh_cred_stage'
+            prod_dh_creds  = 'dh_cred_prod'
+        }
+
+        parameters {
+
+            choice(
+                name: 'account',
+                choices: ['dev', 'qa', 'stage', 'prod'],
+                description: 'Select the environment.'
+            )
+        }
+
+        stages {
+
+            stage('Building the Docker Image in Dev') {
+
+                when {
+                    expression {
+                        params.account == 'dev'
+                    }
+                }
+
+                environment {
+
+                    dev_registry_endpoint =
+                        "${env.registryURL}${env.dev_registry}"
+
+                    dev_image =
+                        "${env.dev_registry}:${GIT_COMMIT}"
+                }
+
+                steps {
+
+                    script {
+
+                        docker.withRegistry(
+                            dev_registry_endpoint,
+                            dev_dh_creds
+                        ) {
+
+                            docker.build(
+                                "${env.dev_image}",
+                                '.'
+                            ).push()
+                        }
+                    }
+                }
+
+                post {
+
+                    always {
+
+                        sh """
+                            docker rmi ${env.dev_image} || true
+                        """
+                    }
+                }
+            }
+
+
+            stage('Push the Docker Image in QA') {
+
+                when {
+                    expression {
+                        params.account == 'qa'
+                    }
+                }
+
+                environment {
+
+                    dev_registry_endpoint =
+                        "${env.registryURL}${env.dev_registry}"
+
+                    qa_registry_endpoint =
+                        "${env.registryURL}${env.qa_registry}"
+
+                    dev_image =
+                        "${env.dev_registry}:${GIT_COMMIT}"
+
+                    qa_image =
+                        "${env.qa_registry}:${GIT_COMMIT}"
+                }
+
+                steps {
+
+                    script {
+
+                        // Login to DEV registry and pull image
+                        docker.withRegistry(
+                            dev_registry_endpoint,
+                            dev_dh_creds
+                        ) {
+
+                            docker.image(
+                                "${env.dev_image}"
+                            ).pull()
+                        }
+
+                        // Tag DEV image as QA image
+                        sh """
+                            docker tag \
+                            ${env.dev_image} \
+                            ${env.qa_image}
+                        """
+
+                        // Login to QA registry and push image
+                        docker.withRegistry(
+                            qa_registry_endpoint,
+                            qa_dh_creds
+                        ) {
+
+                            docker.image(
+                                "${env.qa_image}"
+                            ).push()
+                        }
+                    }
+                }
+
+                post {
+
+                    always {
+
+                        sh """
+                            docker rmi ${env.dev_image} || true
+                            docker rmi ${env.qa_image} || true
+                        """
+                    }
+                }
+            }
+
+
+            stage('Push the Docker Image in STAGE') {
+
+                when {
+                    expression {
+                        params.account == 'stage'
+                    }
+                }
+
+                environment {
+
+                    qa_registry_endpoint =
+                        "${env.registryURL}${env.qa_registry}"
+
+                    stage_registry_endpoint =
+                        "${env.registryURL}${env.stage_registry}"
+
+                    qa_image =
+                        "${env.qa_registry}:${GIT_COMMIT}"
+
+                    stage_image =
+                        "${env.stage_registry}:${GIT_COMMIT}"
+                }
+
+                steps {
+
+                    script {
+
+                        // Pull image from QA
+                        docker.withRegistry(
+                            qa_registry_endpoint,
+                            qa_dh_creds
+                        ) {
+
+                            docker.image(
+                                "${env.qa_image}"
+                            ).pull()
+                        }
+
+                        // Tag QA image as STAGE
+                        sh """
+                            docker tag \
+                            ${env.qa_image} \
+                            ${env.stage_image}
+                        """
+
+                        // Push image to STAGE
+                        docker.withRegistry(
+                            stage_registry_endpoint,
+                            stage_dh_creds
+                        ) {
+
+                            docker.image(
+                                "${env.stage_image}"
+                            ).push()
+                        }
+                    }
+                }
+
+                post {
+
+                    always {
+
+                        sh """
+                            docker rmi ${env.qa_image} || true
+                            docker rmi ${env.stage_image} || true
+                        """
+                    }
+                }
+            }
+
+
+            stage('Push the Docker Image in PROD') {
+
+                when {
+                    expression {
+                        params.account == 'prod'
+                    }
+                }
+
+                environment {
+
+                    stage_registry_endpoint =
+                        "${env.registryURL}${env.stage_registry}"
+
+                    prod_registry_endpoint =
+                        "${env.registryURL}${env.prod_registry}"
+
+                    stage_image =
+                        "${env.stage_registry}:${GIT_COMMIT}"
+
+                    prod_image =
+                        "${env.prod_registry}:${GIT_COMMIT}"
+                }
+
+                steps {
+
+                    script {
+
+                        // Pull image from STAGE
+                        docker.withRegistry(
+                            stage_registry_endpoint,
+                            stage_dh_creds
+                        ) {
+
+                            docker.image(
+                                "${env.stage_image}"
+                            ).pull()
+                        }
+
+                        // Tag STAGE image as PROD
+                        sh """
+                            docker tag \
+                            ${env.stage_image} \
+                            ${env.prod_image}
+                        """
+
+                        // Push image to PROD
+                        docker.withRegistry(
+                            prod_registry_endpoint,
+                            prod_dh_creds
+                        ) {
+
+                            docker.image(
+                                "${env.prod_image}"
+                            ).push()
+                        }
+                    }
+                }
+
+                post {
+
+                    always {
+
+                        sh """
+                            docker rmi ${env.stage_image} || true
+                            docker rmi ${env.prod_image} || true
+                        """
+                    }
+                }
+            }
+        }
+    }
+}
